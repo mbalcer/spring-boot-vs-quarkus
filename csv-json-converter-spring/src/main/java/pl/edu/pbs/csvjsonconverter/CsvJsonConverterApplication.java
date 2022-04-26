8805c79497526b67a89a3e15c3fc28abea8615aa
mbalcer <mateusz75319@gmail.com>
2022-04-26 19:31:38 +0200

    Initialize Spring Boot project

---
+++ csv-json-converter-spring/src/main/java/pl/edu/pbs/csvjsonconverter/CsvJsonConverterApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.csvjsonconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvJsonConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvJsonConverterApplication.class, args);
+    }
+
+}
---
+++ csv-json-converter-spring/src/test/java/pl/edu/pbs/csvjsonconverter/CsvJsonConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.csvjsonconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvJsonConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
