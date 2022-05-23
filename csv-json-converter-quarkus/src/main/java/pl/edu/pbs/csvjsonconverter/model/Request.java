316004b7b115454bb7384dd5aff45f671d950a73
mbalcer <mateusz75319@gmail.com>
2022-05-23 18:53:09 +0200

    [q] Removed lombok and unused dependencies

--- csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/model/Request.java
+++ csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/model/Request.java
@@ -1,14 +1,7 @@
 package pl.edu.pbs.csvjsonconverter.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Arrays;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Request {
     private String path;
     private Separator separator;
@@ -21,5 +14,57 @@ public class Request {
         this.path = PATH_TO_DATA + fileName;
         this.separator = separator;
         this.parseTypes = parseTypes;
+    }
+
+    public Request(String path, Separator separator, boolean parseTypes, String[] titles) {
+        this.path = path;
+        this.separator = separator;
+        this.parseTypes = parseTypes;
+        this.titles = titles;
+    }
+
+    public Request() {
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Separator getSeparator() {
+        return separator;
+    }
+
+    public void setSeparator(Separator separator) {
+        this.separator = separator;
+    }
+
+    public boolean isParseTypes() {
+        return parseTypes;
+    }
+
+    public void setParseTypes(boolean parseTypes) {
+        this.parseTypes = parseTypes;
+    }
+
+    public String[] getTitles() {
+        return titles;
+    }
+
+    public void setTitles(String[] titles) {
+        this.titles = titles;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "path='" + path + '\'' +
+                ", separator=" + separator +
+                ", parseTypes=" + parseTypes +
+                ", titles=" + Arrays.toString(titles) +
+                '}';
     }
 }
