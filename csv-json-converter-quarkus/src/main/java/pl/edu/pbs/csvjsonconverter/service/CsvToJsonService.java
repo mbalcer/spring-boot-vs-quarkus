7bc67826cc141a8227fadd9a0a8d5d1fbf932a7d
mbalcer <mateusz75319@gmail.com>
2022-06-04 10:43:50 +0200

    Added processing file

--- csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/service/CsvToJsonService.java
+++ csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/service/CsvToJsonService.java
@@ -1,6 +1,7 @@
 package pl.edu.pbs.csvjsonconverter.service;
 
 import io.smallrye.mutiny.Uni;
+import org.json.JSONObject;
 import pl.edu.pbs.csvjsonconverter.model.Request;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -18,8 +19,22 @@ public class CsvToJsonService {
     }
 
     public Uni<List<String>> convertCsvToJson(Request request) {
-        return fileService.readFile(request.getPath())
-                .onItem()
-                .transform(lines -> Arrays.stream(lines).collect(Collectors.toList()));
+        Uni<String[]> file = fileService.readFile(request.getPath());
+
+        file.map(lines -> lines[0])
+                .map(line -> line.split(request.getSeparator().getValue()))
+                .subscribe()
+                .with(request::setTitles);
+
+        return file.map(lines -> Arrays.stream(lines)
+                .map(line -> line.split(request.getSeparator().getValue()))
+                .map(values -> {
+                    JSONObject jsonObject = new JSONObject();
+                    for (int i = 0; i < values.length; i++) {
+                        jsonObject.put(request.getTitles()[i], values[i]);
+                    }
+                    return jsonObject.toString();
+                })
+                .collect(Collectors.toList()));
     }
 }
