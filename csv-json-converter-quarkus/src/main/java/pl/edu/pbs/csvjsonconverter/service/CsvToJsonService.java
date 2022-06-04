ef8a68dbe82ef9c3a96c0070e08a9a6d865bca05
mbalcer <mateusz75319@gmail.com>
2022-06-04 12:03:08 +0200

    Changed reading file

--- csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/service/CsvToJsonService.java
+++ csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/service/CsvToJsonService.java
@@ -1,14 +1,12 @@
 package pl.edu.pbs.csvjsonconverter.service;
 
-import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
-import org.json.JSONObject;
 import pl.edu.pbs.csvjsonconverter.model.Request;
-import pl.edu.pbs.csvjsonconverter.util.TypesUtils;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class CsvToJsonService {
@@ -19,43 +17,8 @@ public class CsvToJsonService {
         this.fileService = fileService;
     }
 
-    public Multi<String> convertCsvToJson(Request request) {
-        Multi<String> file = fileService.readFile(request.getPath());
-
-        file.select()
-                .first()
-                .onItem()
-                .transform(line -> line.split(request.getSeparator().getValue()))
-                .subscribe()
-                .with(request::setTitles);
-
-        return file.skip()
-                .first()
-                .onItem()
-                .transform(line -> line.split(request.getSeparator().getValue()))
-                .onItem()
-                .transform(values -> {
-                    JSONObject jsonObject = new JSONObject();
-
-                    Multi.createFrom().range(0, values.length)
-                            .subscribe()
-                            .with(i -> {
-                                String[] titles = request.getTitles();
-                                if (titles != null && titles[i].length() > 0) {
-                                    if (request.isParseTypes() && values[i].equals("null")) {
-                                        jsonObject.put(titles[i], JSONObject.NULL);
-                                    } else if (request.isParseTypes() && TypesUtils.isBoolean(values[i])) {
-                                        jsonObject.put(titles[i], Boolean.parseBoolean(values[i]));
-                                    } else if (request.isParseTypes() && TypesUtils.isNumeric(values[i])) {
-                                        jsonObject.put(titles[i], Double.parseDouble(values[i]));
-                                    } else {
-                                        jsonObject.put(titles[i], values[i]);
-                                    }
-                                }
-                            });
-                    return jsonObject;
-                })
-                .onItem()
-                .transform(JSONObject::toString);
+    public Uni<List<String>> convertCsvToJson(Request request) {
+        return fileService.readFile(request.getPath())
+                .map(lines -> Arrays.stream(lines).collect(Collectors.toList()));
     }
 }
--- csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/service/FileService.java
+++ csv-json-converter-quarkus/src/main/java/pl/edu/pbs/csvjsonconverter/service/FileService.java
@@ -1,11 +1,8 @@
 package pl.edu.pbs.csvjsonconverter.service;
 
-import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
-import io.vertx.core.file.OpenOptions;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.core.buffer.Buffer;
-import io.vertx.mutiny.core.file.AsyncFile;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -17,11 +14,10 @@ public class FileService {
         vertx = Vertx.vertx();
     }
 
-    public Multi<String> readFile(String path) {
-        Uni<AsyncFile> uni = vertx.fileSystem()
-                .open(path, new OpenOptions().setRead(true));
-        return uni.onItem()
-                .transformToMulti(AsyncFile::toMulti)
-                .map(Buffer::toString);
+    public Uni<String[]> readFile(String path) {
+        return vertx.fileSystem()
+                .readFile(path)
+                .map(Buffer::toString)
+                .map(content -> content.split("\n"));
     }
 }
